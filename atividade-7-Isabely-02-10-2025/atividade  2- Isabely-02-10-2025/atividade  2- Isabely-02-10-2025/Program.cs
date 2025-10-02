46b41628554df2a179e08ba9d5d2d0fb715543bf
isabelymartinsseara-hue <isabelymartinsseara@gmail.com>
2025-10-02 16:16:43 -0300

    Add files via upload

---
+++ atividade-7-Isabely-02-10-2025/atividade  2- Isabely-02-10-2025/atividade  2- Isabely-02-10-2025/Program.cs
@@ -0,0 +1,9 @@
+﻿//declaração de variáveis
+
+int nu1;
+
+Console.WriteLine("Digite um número;");
+nu1 = int.Parse(Console.ReadLine());
+
+
+console.Writeline($"o antecessor de {nu1 -1}.");
---
+++ atividade-7-Isabely-02-10-2025/atividade-7-Isabely-02-10-2025/Program.cs
@@ -0,0 +1,16 @@
+﻿ //declaração de variáveis
+
+ int nul;
+
+ Console.WriteLine("Digite um número");
+nul = int.Parse(Console.ReadLine());
+
+if (nul > 10)
+{
+    Console.WriteLine($"O número {nul} é MAIOR QUE 10.");
+}
+else
+{
+    Console.WriteLine($"O número {nul}é MENOR que 10.");
+
+}
