666f6fd758f868f0a28ec17a834c3028b93b40ba
isabelymartinsseara-hue <isabelymartinsseara@gmail.com>
2025-10-07 15:11:39 -0300

    Add files via upload

---
+++ atividade 13- Isabely Martins/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite o primeiro valor: ");
+double a = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("digite o segundo valor: ");
+double b = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Digite o terceiro valor : ");
+double c = double.Parse(Console.ReadLine() ?? "0");
+
+double[] arr = new double[] { a, b, c };
+Array.Sort(arr);
+
+Console.WriteLine("Valores em ordem crescente");
+Console.WriteLine($"{arr[0]}, {arr[1]}, {arr[2]}");
+Console.WriteLine("Pressione Enter para sair ...");
+Console.ReadLine();
+
