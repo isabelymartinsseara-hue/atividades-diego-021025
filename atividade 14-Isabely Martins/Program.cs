a1a77fcaccd007f46597e0fd607f58fa329907d8
isabelymartinsseara-hue <isabelymartinsseara@gmail.com>
2025-10-07 15:12:16 -0300

    Add files via upload

---
+++ atividade 14-Isabely Martins/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite um valor N (maior que 0): ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"\nNumeros de 1 ate {n}:");
+for (int i = 1; i <= n; i++)
+
+    Console.WriteLine(i);
+
+Console.WriteLine("\nPressione Enter para sair...");
+Console.ReadLine();
