0a6c0b178f46c507010c5eb56ed8b638ff16ddad
isabelymartinsseara-hue <isabelymartinsseara@gmail.com>
2025-10-07 15:12:43 -0300

    Add files via upload

---
+++ atividade 15-Isabely Martins/Program.cs
@@ -0,0 +1,6 @@
+﻿//Declaração de variavéveis
+
+Console.WriteLine("Numeros entre 1000 e 2000 que deixam resto 5 na divisão por 11:)");
+for (int i = 1000; i <= 2000; i++)
+    if (i % 11 == 5)
+        Console.WriteLine(i);
