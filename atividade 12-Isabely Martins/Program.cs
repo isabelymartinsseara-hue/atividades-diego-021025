f573b351715ff6e9b8421e34c2e6cc6720b116c7
isabelymartinsseara-hue <isabelymartinsseara@gmail.com>
2025-10-07 15:11:01 -0300

    Add files via upload

---
+++ atividade 12-Isabely Martins/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite o primeiro valor: ");
+double a = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Digite o segundo valor: ");
+double b = double.Parse(Console.ReadLine() ?? "0");
+
+double maior = (a > b) ? a : b;
+
+Console.WriteLine($"O maior valor é: {maior}");
+Console.WriteLine("Pressione Enter para sair...");
+Console.WriteLine();
